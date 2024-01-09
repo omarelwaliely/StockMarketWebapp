dfbad937afddff231f47091b5d9945bfa6708c28
omarelwaliely <77898546+omarelwaliely@users.noreply.github.com>
2024-01-10 00:21:57 +0200

    connect to socket through python

---
+++ service_socket.py
@@ -0,0 +1,17 @@
+import socket
+
+class ServiceSocket:
+    def __init__(self, server_ip, server_port):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((server_ip, server_port))  
+    def connect(self):
+        while True:
+            message = input("Enter message to send: ")
+            if message == 'quit':
+                break
+            self.client_socket.send(message.encode())
+            response = self.client_socket.recv(4096)
+            print("Received:", response.decode())
+        self.client_socket.close()
+svc = ServiceSocket('localhost',54000)
+svc.connect()
